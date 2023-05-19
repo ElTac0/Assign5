dc3872802f0d65934ca93c97c59609dff244abcb
Izak Puente <66390089+ElTac0@users.noreply.github.com>
2023-05-19 09:29:15 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 function new_function()
 {
-    document.getElementById("text_id").style.fontSize="2em";
     if(document.getElementById("FancyShmancy_button").checked==true)
     {
         document.getElementById("text_id").style.fontWeight="bold";
